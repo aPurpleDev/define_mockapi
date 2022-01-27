9fd1558df4cf4158ca38025990c71de2862a8633
aPurpleDev <chemseddine.henni@live.fr>
2022-01-27 17:13:18 +0100

    :hammer: enabled CORS for definition route

--- src/router/api_router.ts
+++ src/router/api_router.ts
@@ -1,17 +1,18 @@
-import express from "express";
+import express, { Response, Request, Router } from "express";
+import cors from 'cors'
 import { fetchDefinitions } from "../service/definition";
 
-const api_router = express.Router();
+const api_router:Router = express.Router();
 
-api_router.get('/', (req, res) => {
+api_router.get('/', (req: Request, res: Response): any => {
     res.send('Base API Boilerplate');
 });
 
-api_router.get('/about', (req, res) => {
+api_router.get('/about', (req: Request, res: Response): any => {
     res.send('Just an API fiddle as Node exercise');
 });
 
-api_router.get('/define/:word', async (req, res) => {
+api_router.get('/define/:word', cors(), async (req: Request, res: Response): Promise<any> => {
     try {
         const definition = await fetchDefinitions(req.params.word);
         if (definition) {
